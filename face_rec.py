0b4fb8aa53decf188e1013f5a39b970a99a780d6
lizardgai4 <70610229+lizardgai4@users.noreply.github.com>
2024-02-25 14:21:51 -0800

    Changed an absolute filepath to a relative filepath

--- face_rec.py
+++ face_rec.py
@@ -15,7 +15,7 @@ def show_warning():
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-javi_image = face_recognition.load_image_file("/Users/javiflores/Desktop/StandUp/known_pictures/javi.png")
+javi_image = face_recognition.load_image_file("./known_pictures/javi.png")
 javi_face_encoding = face_recognition.face_encodings(javi_image)[0]
 
 # Create arrays of known face encodings and their names
